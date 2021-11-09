a74325fb79d0a1d0c5037cf60956a5bb382d663e
petoadam1 <72065651+petoadam1@users.noreply.github.com>
2021-11-09 15:02:11 +0100

    November 9 gyak

---
+++ S0KK3C_1109/xPathS0KK3C.java
@@ -0,0 +1,81 @@
+package xpathstudent1109;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class xPathS0KK3C {
+	private static final String FILENAME = "/home/stud2019/peto8/eclipse-workspace/Adatkezeles_XML-ben/XML-ek/xPathS0KK3C.xml";
+
+	public static void main(String[] args) {
+		
+		try {
+	         //File inputFile = new File("input.txt");
+	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	         DocumentBuilder dBuilder;
+
+	         dBuilder = dbFactory.newDocumentBuilder();
+
+	         Document doc = dBuilder.parse(FILENAME);
+	         doc.getDocumentElement().normalize();
+
+	         XPath xPath =  XPathFactory.newInstance().newXPath();
+
+	         String expression = "/class/student";	        
+	         NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(
+	            doc, XPathConstants.NODESET);
+
+	         for (int i = 0; i < nodeList.getLength(); i++) {
+	            Node nNode = nodeList.item(i);
+	            System.out.println("\nCurrent Element :" + nNode.getNodeName());
+	            
+	            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	               Element eElement = (Element) nNode;
+	               System.out.println("Student ID :" + eElement.getAttribute("id"));
+	               System.out.println("First Name : " 
+	                  + eElement
+	                  .getElementsByTagName("firstname")
+	                  .item(0)
+	                  .getTextContent());
+	               System.out.println("Last Name : " 
+	                  + eElement
+	                  .getElementsByTagName("lastname")
+	                  .item(0)
+	                  .getTextContent());
+	               System.out.println("Nick Name : " 
+	                  + eElement
+	                  .getElementsByTagName("nickname")
+	                  .item(0)
+	                  .getTextContent());
+	               System.out.println("Age : " 
+	                  + eElement
+	                  .getElementsByTagName("age")
+	                  .item(0)
+	                  .getTextContent());
+	            }
+	         }
+	      } catch (ParserConfigurationException e) {
+	         e.printStackTrace();
+	      } catch (SAXException e) {
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } catch (XPathExpressionException e) {
+	         e.printStackTrace();
+	      }
+
+	}
+
+}
